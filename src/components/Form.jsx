3af95f4293580a7333ebb43a6736bca09e9c86fb
Fernando Jimenez <fgjv06@gmail.com>
2023-10-03 16:47:01 -0300

    Discharged div (label & input) added with tailwind classes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -47,6 +47,17 @@ const Form = () => {
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
         </div>
 
+        <div className="mb-5">
+          <label
+          htmlFor="discharged"
+          className="block text-gray-700 uppercase font-bold">Discharged on</label>
+
+          <input
+          id="discharged"
+          type="date"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+        </div>
+
       </form>
     </div>
   )
