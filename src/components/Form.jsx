e5188274091e3ee732d647da08f9a9dd48f28c55
Fernando Jimenez <fgjv06@gmail.com>
2023-10-03 13:40:30 -0300

    p text large, margin top, text centered. span manage them purple and font bold

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,11 @@ const Form = () => {
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Book a vet appointment</h2>
+
+      <p className="text-lg mt-5 text-center">
+        Add Patients and {''}
+        <span className="text-indigo-600 font-bold">Manage them</span>
+      </p>
     </div>
   )
 }
