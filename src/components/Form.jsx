87e7d71d78a74250db2e1c9de03d1a222baa5b7d
Fernando Jimenez <fgjv06@gmail.com>
2023-10-03 14:07:52 -0300

    input id and label htmlFor, this is in case user clicks label to automatically select the input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,9 +10,9 @@ const Form = () => {
 
       <form className="bg-white shadow-md rounded-lg py-10 px-5">
         <div>
-          <label className="block text-gray-700 uppercase font-bold">Pet Name</label>
+          <label htmlFor="pet" className="block text-gray-700 uppercase font-bold">Pet Name</label>
 
-          <input type="text" placeholder="Enter name" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+          <input id="pet" type="text" placeholder="Enter name" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
         </div>
       </form>
     </div>
