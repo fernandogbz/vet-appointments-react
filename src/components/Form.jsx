a7fa12ed317e09638b0c686ea4e0886802c7e082
Fernando Jimenez <fgjv06@gmail.com>
2023-10-03 16:44:07 -0300

    Pet name div (label & input) added with tailwind classes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 const Form = () => {
   return (
     <div className="md:w-1/2 lg:w-2/5">
+
       <h2 className="font-black text-3xl text-center">Book a vet appointment</h2>
 
       <p className="text-lg mt-5 text-center mb-10">
@@ -9,11 +10,19 @@ const Form = () => {
       </p>
 
       <form className="bg-white shadow-md rounded-lg py-10 px-5">
-        <div>
-          <label htmlFor="pet" className="block text-gray-700 uppercase font-bold">Pet Name</label>
 
-          <input id="pet" type="text" placeholder="Enter name" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+        <div className="mb-5">
+          <label 
+          htmlFor="pet"
+          className="block text-gray-700 uppercase font-bold">Pet Name</label>
+
+          <input 
+          id="pet"
+          type="text"
+          placeholder="Enter pet name"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
         </div>
+
       </form>
     </div>
   )
