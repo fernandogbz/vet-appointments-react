fd911bbb25a35675fe7aeca2dc3fc6f58747c082
Fernando Jimenez <fgjv06@gmail.com>
2023-10-03 16:45:35 -0300

    Email div (label & input) added with tailwind classes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -35,6 +35,18 @@ const Form = () => {
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
         </div>
 
+        <div className="mb-5">
+          <label
+          htmlFor="email"
+          className="block text-gray-700 uppercase font-bold">Email</label>
+
+          <input
+          id="email"
+          type="email"
+          placeholder="Email"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+        </div>
+
       </form>
     </div>
   )
