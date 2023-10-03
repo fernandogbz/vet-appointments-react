df3af54265f7a0ab63934d299bd7ebd9d8c223d7
Fernando Jimenez <fgjv06@gmail.com>
2023-10-03 13:57:32 -0300

    p margin bottom

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ const Form = () => {
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Book a vet appointment</h2>
 
-      <p className="text-lg mt-5 text-center">
+      <p className="text-lg mt-5 text-center mb-10">
         Add Patients and {''}
         <span className="text-indigo-600 font-bold">Manage them</span>
       </p>
