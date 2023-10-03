2849f086935482b546cb6a5ecb3e8c3e1342baaa
Fernando Jimenez <fgjv06@gmail.com>
2023-10-03 17:35:22 -0300

    h1 changed to follow up, and 2 <p> below added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 const Header = () => {
   return (
     <>
-      <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-        Seguimiento Pacientes {""}
-        <span className="text-indigo-600">Veterinaria</span>
-        </h1>
+      <h1
+      className="font-black text-5xl text-center md:w-2/3 mx-auto mb-5">
+        Follow up {""}
+        <span 
+        className="text-indigo-600">Veterinary</span> {""}
+        Patients
+      </h1>
+        
+      <p 
+        className="text-lg text-center mb-2 uppercase font-semibold">Trusted care for your pet&apos;s unique needs</p>
+
+      <p
+      className="text-lg text-center mb-10 p-2"
+      >We&apos;re committed to delivering compassionate vet care with a broad range of affordable, personalized solutions.</p>
     </>
   )
 }
