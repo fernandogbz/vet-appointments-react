31753d4bd1f1031d200ff9a7e02489074bbf4042
Fernando Jimenez <fgjv06@gmail.com>
2023-10-04 17:45:33 -0300

    p Manage Patients added with tailwind classes, margins, colors and text centered

--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -1,7 +1,13 @@
 const PatientsList = () => {
   return (
-    <div className="w-1/2 lg:w-3/5">
+    <div className="mid:w-1/2 lg:w-3/5">
+
     <h2 className="font-black text-3xl text-center">Patients List</h2>
+
+    <p className="text-xl mt-5 mb-10 text-center">
+      Manage your {''}
+      <span className="text-indigo-600 font-bold">Patients and Appointments</span>
+    </p>
     </div>
     )
 }
