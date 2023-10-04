22b9e35e83ae89044d32548180a58b6a5a4e70ee
Fernando Jimenez <fgjv06@gmail.com>
2023-10-04 18:19:30 -0300

    Patient information div pasted

---
+++ src/components/Patient.jsx
@@ -0,0 +1,23 @@
+const Patient = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">Pet Name: {''}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Owner Name: {''}
+        <span className="font-normal normal-case">Fer</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Email {''}
+        <span className="font-normal normal-case">email@email.com</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Discharged on: {''}
+        <span className="font-normal normal-case">October 4th, 2023</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Symptoms: {''}
+        <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatibus culpa molestias animi eius vel suscipit placeat optio. Magni excepturi non quisquam nulla!</span>
+      </p>
+    </div>
+  )
+}
+
+export default Patient
