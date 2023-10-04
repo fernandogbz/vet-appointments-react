61d12c343ffa0ee98719ab7c261eca4f12b09aa5
Fernando Jimenez <fgjv06@gmail.com>
2023-10-04 18:00:46 -0300

    patient information p normal font and normal case (Exactly like the user  who added it, wrote it)

--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -11,7 +11,7 @@ const PatientsList = () => {
 
     <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">Name: {''}
-        <span>Hook</span>
+        <span className="font-normal normal-case">Hook</span>
         </p>
     </div>
 
