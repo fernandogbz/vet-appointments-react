46bb09a94f84830ec02cd5750b7ab0caf5e02b15
Fernando Jimenez <fgjv06@gmail.com>
2023-10-08 23:23:52 -0300

    const owner, setOwner = useState('')

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react';
 
 const Form = () => {
   const [name, setName] = useState('');
+  const [owner, setOwner] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
