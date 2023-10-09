3735fd05197d38fc8bc1d3526dd74bc8ad6a2ca5
Fernando Jimenez <fgjv06@gmail.com>
2023-10-08 23:42:47 -0300

    main div marginx-5

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -14,7 +14,7 @@ const Form = () => {
   }
   
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
 
       <h2 className="font-black text-3xl text-center">Book a vet appointment</h2>
 
