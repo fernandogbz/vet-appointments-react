e76f0d02089d3c9165519d4068e6263a56b04233
Fernando Jimenez <fgjv06@gmail.com>
2023-10-27 23:23:35 -0300

    Using Error as a component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -56,7 +56,7 @@ const Form = ( {patients, setPatients} ) => {
       onSubmit={handleSubmit}
       className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
 
-        { error && }
+        { error && <Error/> }
 
         <div className="mb-5">
           <label 
