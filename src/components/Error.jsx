0baf2502ed76560be97b9906c2093ba889d4ce8e
Fernando Jimenez <fgjv06@gmail.com>
2023-11-04 17:26:44 -0300

    All fields are required p added

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
-const Error = () => {
+const Error = ({message}) => {
   return (
     <div className='bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
-      <p>All fields are required</p>
+      <p>{message}</p>
     </div>
   )
 }
