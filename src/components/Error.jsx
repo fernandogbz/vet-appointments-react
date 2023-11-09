02255dc320fb4c2603cdbb19a400a4822f26c1e2
Fernando Jimenez <fgjv06@gmail.com>
2023-11-09 18:41:57 -0300

    passing children prop

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
-const Error = ({message}) => {
+const Error = ({children}) => {
   return (
     <div className='bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
-      <p>{message}</p>
+      <p>{children}</p>
     </div>
   )
 }
