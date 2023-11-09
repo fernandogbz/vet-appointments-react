fa33ae0476c5625c416d5a8824cd2ca68d973e73
Fernando Jimenez <fgjv06@gmail.com>
2023-11-09 18:42:41 -0300

    Error {children} prop

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -56,7 +56,7 @@ const Form = ( {patients, setPatients} ) => {
       onSubmit={handleSubmit}
       className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
 
-        { error && <Error message='All fields are required'/> }
+        { error && <Error>All fields are required</Error> }
 
         <div className="mb-5">
           <label 
