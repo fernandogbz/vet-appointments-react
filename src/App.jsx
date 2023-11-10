b29a5e1b9471fda5209796345ea2b071887f2960
Fernando Jimenez <fgjv06@gmail.com>
2023-11-10 11:45:05 -0300

    Sending patients from App to PatientsList by props

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,9 @@ function App() {
           patients={patients}
           setPatients={setPatients}
         />
-        <PatientsList />
+        <PatientsList 
+          patients={patients}
+        />
       </div>
 
     </div>
