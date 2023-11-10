0248a3b4375baca2c8694f969651edf47037e98c
Fernando Jimenez <fgjv06@gmail.com>
2023-11-10 14:06:47 -0300

    patients.map

--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -1,19 +1,27 @@
 import Patient from "./Patient"
 
 
-const PatientsList = (patients) => {
+const PatientsList = ({patients}) => {
   console.log(patients)
+
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll ">
 
-    <h2 className="font-black text-3xl text-center">Patients List</h2>
+      <h2 className="font-black text-3xl text-center">Patients List</h2>
 
-    <p className="text-xl mt-5 mb-10 text-center">
+      <p className="text-xl mt-5 mb-10 text-center">
       Manage your {''}
-      <span className="text-indigo-600 font-bold">Patients and Appointments</span>
-    </p>
+        <span className="text-indigo-600 font-bold">Patients and Appointments</span>
+      </p>
 
-    <Patient />
+      { patients.map( () => (
+      
+        <h1>Desde map</h1>
+        )
+      ) 
+    }
+    
+      <Patient />
     </div>
     )
 }
