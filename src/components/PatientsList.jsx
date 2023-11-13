c2e629e65211ddf28f867f7bb0571e7a7e0e440b
Fernando Jimenez <fgjv06@gmail.com>
2023-11-13 19:29:01 -0300

    condition ternary operator, if patients and patients.length have some value indicate that'there are patients', otherwise indicate that 'there are no patients'

--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -7,6 +7,8 @@ const PatientsList = ({patients}) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll ">
 
+    {patients && patients.length ? "There are patients" : "There are no patients"}
+
       <h2 className="font-black text-3xl text-center">Patients List</h2>
 
       <p className="text-xl mt-5 mb-10 text-center">
