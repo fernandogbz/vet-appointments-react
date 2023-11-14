8c755d1d1b6a2cf8b3316f23e4453152321c18a6
Fernando Jimenez <fgjv06@gmail.com>
2023-11-14 19:16:45 -0300

    buttons div styles

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -21,7 +21,7 @@ const Patient = ({patient}) => {
         <span className="font-normal normal-case">{symptoms}</span>
       </p>
 
-      <div>
+      <div className="flex justify-between mt-10">
         <button
           type="button"
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
