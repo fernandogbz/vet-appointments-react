9212736c2754f213dbbbf772242c9f92ccad2005
Fernando Jimenez <fgjv06@gmail.com>
2023-11-14 19:15:07 -0300

    Delete button styles

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -27,9 +27,9 @@ const Patient = ({patient}) => {
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
         >Edit</button>
 
-
         <button
           type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
           >Delete</button>
       </div>
     </div>
