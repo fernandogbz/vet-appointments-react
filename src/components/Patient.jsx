19d8c1e07a8086145514eb73f7d763b32317761e
Fernando Jimenez <fgjv06@gmail.com>
2023-11-15 19:17:48 -0300

    Edit button on click passes the setPatient object to App to modify it

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -25,6 +25,7 @@ const Patient = ({patient, setPatient}) => {
         <button
           type="button"
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => setPatient(patient)}
         >Edit</button>
 
         <button
