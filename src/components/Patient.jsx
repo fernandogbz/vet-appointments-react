8f46f5c952112907c9d0670021cde8853d634d6b
Fernando Jimenez <fgjv06@gmail.com>
2023-11-20 17:55:27 -0300

    handleDelete function created

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -2,6 +2,10 @@ const Patient = ({patient, setPatient, deletePatient}) => {
 
   const {name, owner, email, date, symptoms, id} = patient // Destructuring patient object
 
+  const handleDelete = () => {
+    console.log('Deleting', id)
+  }
+
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">Pet Name: {''}
