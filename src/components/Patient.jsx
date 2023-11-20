e65ae7954c3a078c3d5d17f25d5a41e9f875c8a2
Fernando Jimenez <fgjv06@gmail.com>
2023-11-20 17:52:50 -0300

    Delete button onclick calls handleDelete

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -31,7 +31,7 @@ const Patient = ({patient, setPatient, deletePatient}) => {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
-          onClick={() => deletePatient(id)}
+          onClick={handleDelete}
 
           >Delete</button>
       </div>
