10ecf061db43122b3db1b447f49b16b730f80928
Fernando Jimenez <fgjv06@gmail.com>
2023-11-20 17:33:31 -0300

    Delete button onclick deletePatient() function

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -31,6 +31,8 @@ const Patient = ({patient, setPatient, deletePatient}) => {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => deletePatient()}
+
           >Delete</button>
       </div>
     </div>
